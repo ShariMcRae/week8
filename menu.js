f68d9eb6c9d70ac69fe5c13bb8f7c118138930a1
Shari McRae <sharimcrae@protonmail.com>
2023-02-05 08:37:27 -0600

    added week7 code

---
+++ menu.js
@@ -0,0 +1,19 @@
+document.write(`
+    <img class="banner" src="images/banner.png" alt="Health and fitness banner."/>
+    <h1 class="header">Strategic Nutrition for Fitness</h1>
+    <table class="menu">
+    <tr>
+        <th>
+            <h2><a href="index.html">Home</a></h2>
+        </th>
+
+        <th>
+            <h2><a href="programs.html">Programs</a></h2>
+        </th>
+
+        <th>
+            <h2><a href="contact.html">Contact</a></h2>
+        </th>
+    </tr>
+    </table>
+`);
