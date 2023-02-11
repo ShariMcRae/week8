90e6ad26a68cddd195d51d436a307784f48ff72e
Shari McRae <sharimcrae@protonmail.com>
2023-02-11 07:05:03 -0600

    minor changes to code layout and comments

--- menu.js
+++ menu.js
@@ -1,5 +1,9 @@
 document.write(`
-    <div class="banner"><div><img src="images/banner.png" alt="Health and fitness banner."/></div></div>
+    <div class="banner">
+        <div>
+            <img src="images/banner.png" alt="Health and fitness banner."/>
+        </div>
+    </div>
     <h1 class="header">Strategic Nutrition for Fitness</h1>
     <div class="menu">
         <div><a href="index.html">Home</a></div>
